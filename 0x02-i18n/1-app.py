826f2e1a758f24f5da16d5405b009b6f4bcc9ac9
mariams58 <mariams58@yahoo.com>
2024-11-11 13:15:59 +0100

    fix: i18n task 1

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""Hello Holberton Flask Module"""
+
+from flask import Flask, render_template
+from flask_babel import Babel
+from config import Config
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.route("/", methods=['GET'], strict_slashes=False)
+def home() -> str:
+    """ Define the route for home
+        Return 1-index.html
+    """
+    return render_template("1-index.html")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000, debug=True)
---
+++ 0x02-i18n/config.py
@@ -0,0 +1,8 @@
+"""Config file for i1n8 task 1"""
+
+
+class Config:
+    """Config class for task 1"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
